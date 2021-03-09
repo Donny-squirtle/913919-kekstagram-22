6e6eaa5a2ee00ee745ac73d8c3b205c7911f70cc
Donny Squirtle <desternub@gmail.com>
2021-03-09 19:34:00 +0300

    Ещё правки

--- js/picture.js
+++ js/picture.js
@@ -3,18 +3,21 @@ import { getPhotosData } from './users.js';
 
 const allUserPicture = document.querySelector('.pictures');
 const picture = document.querySelector('#picture');
-const arr = getPhotosData();
-const getUsersPictures = () => {
-  for (let i = 1; i < arr.length; i++) {
-    const userPicture = picture.content.cloneNode(true);
-    const userPictureImage = userPicture.querySelector('.picture__img');
-    const userPictureComments = userPicture.querySelector('.picture__comments');
-    const userPictureLikes = userPicture.querySelector('.picture__likes');
-    allUserPicture.appendChild(userPicture);
-    userPictureImage.setAttribute('src', arr[i].url);
-    userPictureComments.textContent = arr[i].comments.message;
-    userPictureLikes.textContent = arr[i].likes;
-  }
-};
-getUsersPictures();
-
+const userInformation = getPhotosData();
+//const renderUsersPictures = () => {
+//   for (let i = 1; i < userInformation.length; i++) {
+//     const userPicture = picture.content.cloneNode(true);
+//     allUserPicture.appendChild(userPicture);
+//     userPicture.querySelector('.picture__img').setAttribute('src', userInformation[i].url);
+//     userPicture.querySelector('.picture__comments').textContent = userInformation[i].comments.message;
+//     userPicture.querySelector('.picture__likes').textContent = userInformation[i].likes;
+//   }
+// };
+//renderUsersPictures();
+userInformation.forEach((user) => {
+  const userPicture = picture.content.cloneNode(true);
+  userPicture.querySelector('.picture__img').setAttribute('src', user.url);
+  userPicture.querySelector('.picture__comments').textContent = user.comments.message;
+  userPicture.querySelector('.picture__likes').textContent = user.likes;
+  allUserPicture.appendChild(userPicture);
+});
