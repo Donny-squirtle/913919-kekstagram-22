02ecf14c316d861102dc823ddbcb9ba8c835dfbd
burtest <sannnnna@bk.ru>
2021-03-09 14:03:07 +0300

    Правки

--- js/picture.js
+++ js/picture.js
@@ -4,17 +4,14 @@ import { getPhotosData } from './users.js';
 const allUserPicture = document.querySelector('.pictures');
 const picture = document.querySelector('#picture');
 const arr = getPhotosData();
-const getUsersPictures = () => {
+const renderUsersPictures = () => {
   for (let i = 1; i < arr.length; i++) {
     const userPicture = picture.content.cloneNode(true);
-    const userPictureImage = userPicture.querySelector('.picture__img');
-    const userPictureComments = userPicture.querySelector('.picture__comments');
-    const userPictureLikes = userPicture.querySelector('.picture__likes');
     allUserPicture.appendChild(userPicture);
-    userPictureImage.setAttribute('src', arr[i].url);
-    userPictureComments.textContent = arr[i].comments.message;
-    userPictureLikes.textContent = arr[i].likes;
+    userPicture.querySelector('.picture__img').setAttribute('src', arr[i].url);
+    userPicture.querySelector('.picture__comments').textContent = arr[i].comments.message;
+    userPicture.querySelector('.picture__likes').textContent = arr[i].likes;
   }
 };
-getUsersPictures();
+renderUsersPictures();
 
